3630f4cbb250e347278043489e068104fae4c6c1
Matt Bator <mattbator@users.noreply.github.com>
2018-01-22 16:49:01 -0500

    More theme fixes

--- conf.py
+++ conf.py
@@ -147,7 +147,7 @@ html_theme_options = {
 
     # HTML navbar class (Default: "navbar") to attach to <div> element.
     # For black navbar, do "navbar navbar-inverse"
-    'navbar_class': "navbar navbar-inverse",
+    'navbar_class': "navbar-inverse",
 
     # Fix navigation bar to top of page?
     # Values: "true" (default) or "false"
